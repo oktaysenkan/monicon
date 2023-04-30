a5ec9bf4e5c57a3044e2d0395d4aa8c50478b6af
Oktay Şenkan <oktay.senkan@gmail.com>
2023-04-30 09:40:00 +0300

    docs: add bundle size section

--- README.md
+++ README.md
@@ -61,6 +61,16 @@ export default ExampleScreen;
 
 In the example above, we show how to use the mdi-heart Iconify icon. You can provide values for the size and color props to customize the appearance of the icon.
 
+## Bundle size
+
+Tested on empty expo managed app
+
+| Icons | Size (MB) | Difference (MB) |
+| ----- | --------- | --------------- |
+| 1     | 3.07      | -               |
+| 100   | 3.14      | +0.07           |
+| 1000  | 3.7       | +0.63           |
+
 ## Troubleshooting
 
 ### Iconify: 'icon' prop must be a string literal
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,11 +5,134 @@ import { Iconify } from 'react-native-iconify';
 export default function App() {
   return (
     <SafeAreaView style={$flex}>
-      <Iconify icon="mdi:heart" size={32} color="red" />
+      <Iconify icon="mdi:heart" size={32} color="black" />
+      <Iconify icon="mdi:ab-testing" size={32} color="black" />
+      <Iconify icon="mdi:abacus" size={32} color="black" />
+      <Iconify icon="mdi:abjad-arabic" size={32} color="black" />
+      <Iconify icon="mdi:abjad-hebrew" size={32} color="black" />
+      <Iconify icon="mdi:abugida-devanagari" size={32} color="black" />
+      <Iconify icon="mdi:abugida-thai" size={32} color="black" />
+      <Iconify icon="mdi:access-point" size={32} color="black" />
+      <Iconify icon="mdi:access-point-check" size={32} color="black" />
+      <Iconify icon="mdi:access-point-minus" size={32} color="black" />
+      <Iconify icon="mdi:access-point-network" size={32} color="black" />
+      <Iconify icon="mdi:access-point-network-off" size={32} color="black" />
+      <Iconify icon="mdi:access-point-off" size={32} color="black" />
+      <Iconify icon="mdi:access-point-plus" size={32} color="black" />
+      <Iconify icon="mdi:access-point-remove" size={32} color="black" />
+      <Iconify icon="mdi:account" size={32} color="black" />
+      <Iconify icon="mdi:account-alert" size={32} color="black" />
+      <Iconify icon="mdi:account-alert-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-down" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-down-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-left" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-left-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-right" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-right-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-up" size={32} color="black" />
+      <Iconify icon="mdi:account-arrow-up-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-badge" size={32} color="black" />
+      <Iconify icon="mdi:account-badge-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-box" size={32} color="black" />
+      <Iconify icon="mdi:account-box-multiple" size={32} color="black" />
+      <Iconify
+        icon="mdi:account-box-multiple-outline"
+        size={32}
+        color="black"
+      />
+      <Iconify icon="mdi:account-box-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-cancel" size={32} color="black" />
+      <Iconify icon="mdi:account-cancel-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-card" size={32} color="black" />
+      <Iconify icon="mdi:account-card-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-cash" size={32} color="black" />
+      <Iconify icon="mdi:account-cash-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-check" size={32} color="black" />
+      <Iconify icon="mdi:account-check-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-child" size={32} color="black" />
+      <Iconify icon="mdi:account-child-circle" size={32} color="black" />
+      <Iconify icon="mdi:account-child-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-circle" size={32} color="black" />
+      <Iconify icon="mdi:account-circle-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-clock" size={32} color="black" />
+      <Iconify icon="mdi:account-clock-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-cog" size={32} color="black" />
+      <Iconify icon="mdi:account-cog-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-convert" size={32} color="black" />
+      <Iconify icon="mdi:account-convert-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-cowboy-hat" size={32} color="black" />
+      <Iconify icon="mdi:account-cowboy-hat-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-credit-card" size={32} color="black" />
+      <Iconify icon="mdi:account-credit-card-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-details" size={32} color="black" />
+      <Iconify icon="mdi:account-details-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-edit" size={32} color="black" />
+      <Iconify icon="mdi:account-edit-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-eye" size={32} color="black" />
+      <Iconify icon="mdi:account-eye-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-filter" size={32} color="black" />
+      <Iconify icon="mdi:account-filter-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-group" size={32} color="black" />
+      <Iconify icon="mdi:account-group-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-hard-hat" size={32} color="black" />
+      <Iconify icon="mdi:account-hard-hat-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-heart" size={32} color="black" />
+      <Iconify icon="mdi:account-heart-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-injury" size={32} color="black" />
+      <Iconify icon="mdi:account-injury-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-key" size={32} color="black" />
+      <Iconify icon="mdi:account-key-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-lock" size={32} color="black" />
+      <Iconify icon="mdi:account-lock-open" size={32} color="black" />
+      <Iconify icon="mdi:account-lock-open-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-lock-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-minus" size={32} color="black" />
+      <Iconify icon="mdi:account-minus-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-multiple" size={32} color="black" />
+      <Iconify icon="mdi:account-multiple-check" size={32} color="black" />
+      <Iconify
+        icon="mdi:account-multiple-check-outline"
+        size={32}
+        color="black"
+      />
+      <Iconify icon="mdi:account-multiple-minus" size={32} color="black" />
+      <Iconify
+        icon="mdi:account-multiple-minus-outline"
+        size={32}
+        color="black"
+      />
+      <Iconify icon="mdi:account-multiple-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-multiple-plus" size={32} color="black" />
+      <Iconify
+        icon="mdi:account-multiple-plus-outline"
+        size={32}
+        color="black"
+      />
+      <Iconify icon="mdi:account-multiple-remove" size={32} color="black" />
+      <Iconify
+        icon="mdi:account-multiple-remove-outline"
+        size={32}
+        color="black"
+      />
+      <Iconify icon="mdi:account-music" size={32} color="black" />
+      <Iconify icon="mdi:account-music-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-network" size={32} color="black" />
+      <Iconify icon="mdi:account-network-off" size={32} color="black" />
+      <Iconify icon="mdi:account-network-off-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-network-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-off" size={32} color="black" />
+      <Iconify icon="mdi:account-off-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-plus" size={32} color="black" />
+      <Iconify icon="mdi:account-plus-outline" size={32} color="black" />
+      <Iconify icon="mdi:account-question" size={32} color="black" />
     </SafeAreaView>
   );
 }
 
 const $flex: ViewStyle = {
   flex: 1,
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  gap: 8,
 };
