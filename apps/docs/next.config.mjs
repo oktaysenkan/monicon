d939d28be294e99aba1ba49225f2a109446e86a8
Oktay Şenkan <oktay.senkan@gmail.com>
2024-10-06 03:44:08 +0300

    docs(docs): add installation doc for nuxt apps

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,8 +1,17 @@
 import nextra from "nextra";
+import webpack from "webpack";
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-export default withNextra();
+export default withNextra({
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins.push(new webpack.ContextReplacementPlugin(/twoslash/));
+
+    return config;
+  },
+});
---
+++ apps/docs/pages/installation/_meta.tsx
@@ -0,0 +1,6 @@
+export default {
+  react: "React",
+  "react-native": "React Native",
+  vue: "Vue",
+  nuxt: "Nuxt",
+};
