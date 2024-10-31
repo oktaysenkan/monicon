96c1607666c93bffa2755872e64a81f9c1341421
Oktay Şenkan <oktay.senkan@gmail.com>
2024-10-31 05:34:57 +0300

    fix(docs): add missing og meta tags

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -21,6 +21,8 @@ const config: DocsThemeConfig = {
       <>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://monicon.vercel.app" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={ogImage} />
@@ -33,6 +35,8 @@ const config: DocsThemeConfig = {
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={ogImage} />
+        <meta property="twitter:domain" content="monicon.vercel.app" />
+        <meta property="twitter:url" content="https://monicon.vercel.app" />
       </>
     );
   },
