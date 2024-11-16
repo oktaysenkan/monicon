46d522fbac2afcb29325676f9e7f233ba3e90d85
Oktay Şenkan <oktay.senkan@gmail.com>
2024-11-16 20:12:32 +0300

    feat(loader): implement error handling

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -3,6 +3,8 @@ import { readFileSync } from "fs";
 import slugify from "slugify";
 import * as _ from "radashi";
 import path from "path";
+import * as f from "fuuu";
+import { parseSync } from "svgson";
 
 slugify.extend({ "/": "-" });
 
@@ -19,16 +21,44 @@ export type JSONLoaderOptions =
       options?: RequestInit;
     };
 
+type Content = {
+  name: string;
+  content: string;
+};
+
+const isValidSvg = (svg: string) => {
+  const parsed = f.syncSafe(() => parseSync(svg));
+
+  return !parsed.error;
+};
+
 export const JSONLoader: Loader<JSONLoaderOptions> = (input) => async () => {
   const url = typeof input === "string" ? input : input.url;
 
   const options = typeof input === "string" ? undefined : input.options;
 
-  const response = await fetch(url, options);
+  const response = await f.safe(() => fetch(url, options));
 
-  const content = await response.json();
+  if (response.error) {
+    console.warn(`[Monicon] Request to "${url}" failed.`);
+    return {};
+  }
 
-  return content as LoaderResult;
+  const content = await f.safe<LoaderResult>(() => response.data.json());
+
+  if (content.error) {
+    console.warn(`[Monicon] Unable to parse response from "${url}".`);
+    return {};
+  }
+
+  Object.entries(content.data).forEach(([key, value]) => {
+    if (!isValidSvg(value)) {
+      console.warn(`[Monicon] The response from "${url}" is not a valid SVG.`);
+      delete content.data[key];
+    }
+  });
+
+  return content.data as LoaderResult;
 };
 
 export type RemoteLoaderOptions = Record<
@@ -48,18 +78,37 @@ export const remoteLoader: Loader<RemoteLoaderOptions> =
 
         const options = typeof value === "string" ? undefined : value.options;
 
-        const response = await fetch(url, options);
+        const response = await f.safe(() => fetch(url, options));
 
-        const content = await response.text();
+        if (response.error) {
+          console.warn(`[Monicon] Request to "${url}" failed.`);
+          return;
+        }
 
-        return { name: key, content };
+        const content = await f.safe(() => response.data.text());
+
+        if (content.error) {
+          console.warn(`[Monicon] Unable to parse response from "${url}".`);
+          return;
+        }
+
+        if (!isValidSvg(content.data)) {
+          console.warn(
+            `[Monicon] The response from "${url}" is not a valid SVG.`
+          );
+          return;
+        }
+
+        return { name: key, content: content.data } as Content;
       })
     );
 
+    const asArrayFiltered = asArray.filter((item) => !!item) as Content[];
+
     const asObject = _.objectify(
-      asArray,
-      (f) => f.name,
-      (f) => f.content
+      asArrayFiltered,
+      (item) => item.name,
+      (item) => item.content
     );
 
     return asObject as LoaderResult;
@@ -80,8 +129,26 @@ export const localLoader: Loader<LocalLoaderOptions> = (input) => () => {
 
   const filePaths = glob.sync(`${directory}/**/*.svg`);
 
+  if (!filePaths.length) {
+    console.warn(
+      `[Monicon] No files were found in the directory "${directory}".`
+    );
+  }
+
   const files = filePaths.map((filePath) => {
-    const content = readFileSync(filePath, "utf-8");
+    const content = f.syncSafe(() => readFileSync(filePath, "utf-8"));
+
+    if (content.error) {
+      console.warn(
+        `[Monicon] The file "${filePath}" was not found. This file might not exist, or the required icon file might not be in the correct format.`
+      );
+      return;
+    }
+
+    if (!isValidSvg(content.data)) {
+      console.warn(`[Monicon] The file "${filePath}" is not a valid SVG.`);
+      return;
+    }
 
     const fileAbsolutePath = path.resolve(filePath);
 
@@ -94,13 +161,15 @@ export const localLoader: Loader<LocalLoaderOptions> = (input) => () => {
 
     const slugified = slugify(fileNameWithoutExtension, { lower: true });
 
-    return { name: slugified, content };
+    return { name: slugified, content: content.data } as Content;
   });
 
+  const filesFiltered = files.filter((item) => !!item) as Content[];
+
   const asObject = _.objectify(
-    files,
-    (f) => f.name,
-    (f) => f.content
+    filesFiltered,
+    (item) => item.name,
+    (item) => item.content
   );
 
   return asObject;
