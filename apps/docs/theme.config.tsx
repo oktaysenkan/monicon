a0591b3b1235545f58e4df94c39650df8309c381
Oktay Şenkan <oktay.senkan@gmail.com>
2024-11-17 21:58:27 +0300

    feat(docs): add favicon

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
     const config = useConfig();
     const { route, basePath } = useRouter();
 
-    const site = basePath || "https://monicon.vercel.app";
+    const site = basePath || "https://monicon-docs.vercel.app";
 
     const description =
       config.frontMatter.description ??
@@ -22,7 +22,7 @@ const config: DocsThemeConfig = {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://monicon.vercel.app" />
+        <meta property="og:url" content="https://monicon-docs.vercel.app" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={ogImage} />
@@ -35,8 +35,84 @@ const config: DocsThemeConfig = {
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={ogImage} />
-        <meta property="twitter:domain" content="monicon.vercel.app" />
-        <meta property="twitter:url" content="https://monicon.vercel.app" />
+        <meta property="twitter:domain" content="monicon-docs.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://monicon-docs.vercel.app"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#000000" />
       </>
     );
   },
