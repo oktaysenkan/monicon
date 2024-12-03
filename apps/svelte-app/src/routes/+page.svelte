82fb3b09cbfa74cce0d59ea7999bc87936059de3
Oktay Şenkan <42527467+oktaysenkan@users.noreply.github.com>
2024-12-03 20:02:37 +0300

    feat(svelte): add svelte 5 support (#56)
    * feat(svelte): add svelte 5 support (#55)
    
    * fix(svelte-app): typo
    
    * docs: add release notes

--- apps/svelte-app/src/routes/+page.svelte
+++ apps/svelte-app/src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script>
-  import { Monicon } from "@monicon/svelte";
+	import { Monicon } from '@monicon/svelte';
 </script>
 
 <main>
-  <Monicon name="mdi:home" size={24} />
-  <Monicon name="logos:active-campaign" />
-  <Monicon name="logos:apache-superset-icon" />
-  <Monicon name="invalid:icon" color="red" />
+	<Monicon name="mdi:home" size={24} />
+	<Monicon name="logos:active-campaign" />
+	<Monicon name="logos:apache-superset-icon" />
+	<Monicon name="invalid:icon" color="red" />
 </main>
 
 <style>
-  :global(html),
-  :global(body) {
-    margin: 0;
-    padding: 0;
-  }
+	:global(html),
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
 
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #242424;
-    color: rgba(255, 255, 255, 0.87);
-  }
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		background-color: #242424;
+		color: rgba(255, 255, 255, 0.87);
+	}
 </style>
