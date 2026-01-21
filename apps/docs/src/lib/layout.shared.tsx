600cabc02d888d8a0a0e7b06eb2fae99af2c01b9
Oktay Şenkan <oktay.senkan@gmail.com>
2026-01-21 19:51:34 +0300

    docs: add logo

--- apps/docs/src/lib/layout.shared.tsx
+++ apps/docs/src/lib/layout.shared.tsx
@@ -1,9 +1,10 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "Monicon",
+      title: <Image src="/logo.svg" alt="Monicon" width={30} height={30} />,
     },
     githubUrl: "https://github.com/oktaysenkan/monicon",
   };
