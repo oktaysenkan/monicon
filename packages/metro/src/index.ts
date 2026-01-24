c18415dcf67a0525b177ccb280b74a7668740a90
Oktay Şenkan <oktay.senkan@gmail.com>
2026-01-24 15:04:07 +0300

    feat: change default metro development mode check

--- packages/metro/src/index.ts
+++ packages/metro/src/index.ts
@@ -1,12 +1,15 @@
 import type { IntermediateConfigT } from "metro-config";
 import { bootstrap, MoniconConfig } from "@monicon/core";
 
+// @ts-expect-error __DEV__ is not defined in the globalThis object
+const isDevelopmentMode = typeof __DEV__ !== "undefined" ? __DEV__ : false;
+
 export const withMonicon = (
   metroConfig: IntermediateConfigT,
   config?: MoniconConfig
 ): IntermediateConfigT => {
   void bootstrap({
-    watch: process.env.BUILD_MODE === "development",
+    watch: isDevelopmentMode,
     ...config,
   });
 
